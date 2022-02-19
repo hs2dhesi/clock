eae8e87d2e426307693ceebc06ca141d05fbecaa
hs2dhesi <hs2dhesi@uwaterloo.ca>
2022-02-18 19:18:00 -0500

    clock app

---
+++ clock.js
@@ -0,0 +1,23 @@
+
+
+setInterval(setClock, 1000)  //we want to call a function every second
+
+const hourHand = document.querySelector('[data-hour-hand]')
+const minuteHand = document.querySelector('[data-minute-hand]')  //we need to use seconds ratio cause we want to increase gradually
+const secondHand = document.querySelector('[data-second-hand]')
+
+function setClock() {
+  const currentDate = new Date()
+  const secondsRatio = currentDate.getSeconds() / 60
+  const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60
+  const hoursRatio = (minutesRatio + currentDate.getHours()) / 12
+  setRotation(secondHand, secondsRatio)
+  setRotation(minuteHand, minutesRatio)
+  setRotation(hourHand, hoursRatio)
+}
+
+function setRotation(element, rotationRatio) {
+  element.style.setProperty('--rotation', rotationRatio * 360)
+}
+
+setClock()
